fd6ac11e4fbf4f4e2f32fb1bc34f2f05503838da
User -2023GXMIC\Administrator <dltjdwns2323@gmail.com>
2023-07-18 11:30:57 +0900

    인덱스 맵핑

---
+++ busJava/src/main/java/config/CtrlConfig.java
@@ -0,0 +1,18 @@
+package config;
+
+import org.springframework.context.annotation.*;
+import ctrl.*;
+
+@Configuration
+public class CtrlConfig {
+// 구현해 놓은 컨트롤러들을 스프링 빈으로 등록시키는 클래스
+	@Bean
+	public IndexCtrl indexCtrl() {
+		return new IndexCtrl();
+	}
+	
+	@Bean
+	public PayChargeCtrl payChargeCtrl() {
+		return new PayChargeCtrl();
+	}
+}
---
+++ busJava/src/main/java/ctrl/PayChargeCtrl.java
@@ -0,0 +1,13 @@
+package ctrl;
+
+import javax.servlet.http.HttpServletRequest;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class PayChargeCtrl {
+	@GetMapping("/test2")
+	public String payCharge() {
+		return "paymoney_charge";
+	}
+}
