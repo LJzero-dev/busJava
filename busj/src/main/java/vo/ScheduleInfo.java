f5ec665812459c2ac37a217762c04d0d0d8ff7e3
배성언 <believeon79@gmail.com>
2023-07-29 01:12:39 +0900

    [사용자] 버스 시간표 vo 불필요 데이터 삭제

--- busj/src/main/java/vo/ScheduleInfo.java
+++ busj/src/main/java/vo/ScheduleInfo.java
@@ -3,7 +3,7 @@ package vo;
 public class ScheduleInfo {
 	private int bs_idx,	bl_idx,	bi_idx;
 	private String bs_stime, bs_etime, bs_date;
-	private int level, company, aprice, aseat, lseat;	// 버스등급, 버스회사, 어른금액, 전체좌석, 예매가능좌석(live-seat)
+
 	public int getBs_idx() {
 		return bs_idx;
 	}
