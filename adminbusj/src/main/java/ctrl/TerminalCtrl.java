f004fb52b1cf4cc900abc8b4b9e856288f1b4481
lsj2323 <dltjdwns2323@gmail.com>
2023-08-01 21:52:29 +0900

    <어드민>시간표 추가 완료

--- adminbusj/src/main/java/ctrl/TerminalCtrl.java
+++ adminbusj/src/main/java/ctrl/TerminalCtrl.java
@@ -121,8 +121,21 @@ public class TerminalCtrl {
 	@PostMapping("/AddLine")		// 노선 추가 insert 작업
 	public String AddLine(HttpServletRequest request, HttpServletResponse response) throws Exception {
 		request.setCharacterEncoding("utf-8");
+		response.setContentType("text/html; charset=utf-8");
+		PrintWriter out = response.getWriter();
+		
 		int bt_sidx = Integer.parseInt(request.getParameter("bt_sidx"));
 		String tmp = request.getParameter("bt_eidx");
+		
+		if(tmp.equals("")) {
+			out.println("<script>");
+			out.println("alert('터미널명을 확인해주세요.');");
+			out.println("history.back();");
+			out.println("</script>");
+			out.close();
+			return "";
+		}
+		
 		String[] arr = tmp.split(":");
 		int bt_eidx = Integer.parseInt(arr[1]);
 		String bt_name = request.getParameter("bt_name");
@@ -132,12 +145,18 @@ public class TerminalCtrl {
 			str += tmp2[i];
 		}
 		int adult = Integer.parseInt(str);
+		if(adult < 10000) {
+			out.println("<script>");
+			out.println("alert('요금을 확인해주세요.');");
+			out.println("history.back();");
+			out.println("</script>");
+			out.close();
+			return "";
+		}
 		
 		int result = terminalSvc.AddLineIn(bt_sidx, bt_eidx, adult);
 		
 		if (result != 1) {
-			response.setContentType("text/html; charset=utf-8");
-			PrintWriter out = response.getWriter();
 			out.println("<script>");
 			out.println("alert('노선 추가에 실패했습니다.');");
 			out.println("history.back();");
@@ -157,17 +176,31 @@ public class TerminalCtrl {
 		int bl_idx = Integer.parseInt(request.getParameter("bl_idx"));
 		String time1 = request.getParameter("time1");
 		String time2 = request.getParameter("time2");
+		
+		int n1 = Integer.parseInt(time1.replace(":", "")) + 100;
+		int n2 = Integer.parseInt(time2.replace(":", ""));
+		response.setContentType("text/html; charset=utf-8");
+		PrintWriter out = response.getWriter();
+		if (n1 > n2) {
+			out.println("<script>");
+			out.println("alert('시간을 확인해주세요.');");
+			out.println("history.back();");
+			out.println("</script>");
+			out.close();
+			return "";
+		}
+		
 		String str = request.getParameter("number");
 		String[] arr = str.split(":");
 		String number = arr[1];
-		response.setContentType("text/html; charset=utf-8");
-		PrintWriter out = response.getWriter();
+		
 		if (number.equals("")) {
 			out.println("<script>");
 			out.println("alert('차량 번호를 선택해주세요.');");
 			out.println("history.back();");
 			out.println("</script>");
 			out.close();
+			return "";
 		}
 		int adult = Integer.parseInt(request.getParameter("adult" + bl_idx));
 		
